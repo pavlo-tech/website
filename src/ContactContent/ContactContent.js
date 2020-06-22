449fe8adb01c57c13e006c08eb737c9d9b4ce1f6
Pavlo Triantafyllides <triantafyllides@gmail.com>
2020-06-22 13:41:42 -0400

    silly contact working

--- src/ContactContent/ContactContent.js
+++ src/ContactContent/ContactContent.js
@@ -4,18 +4,30 @@ import pic from './self.jpeg';
 
 const ContactContent = (props) =>
 {
+  //<h1>Feel free to contact me!</h1>
+
+
   return (
     <div className="ContactContent">
-      <h3>Feel free to contact me:</h3>
-      
-      <img height="100" width="100" src={pic} alt="smiley face"/>
+      <h1 id='T0'>Feel</h1>
+      <h1 id='T1'>free</h1>
+      <h1 id='T2'>to</h1>
+      <h1 id='T3'>cont</h1>
+      <h1 id='T4'>act</h1>
+      <h1 id='T5'>me!</h1>
 
+      
+      <img src={pic} alt="really good pic of me"/>
+
+ 
+      <h2>
+        Pavlo Triantafyllides
+      </h2>
       <p>
-          Pavlo Triantafyllides
-      </p>
-      <p>GitHub: <a href="https://github.com/pavlo-tech" target="__blank">pavlo-tech</a></p>
-      <p>Linkedin: <a href="https://www.linkedin.com/in/pavlo-triantafyllides/" target="__blank">pavlo-triantafyllides</a></p>
-      <p> Email: <a href="mailto:triantafyllides@gmail.com">triantafyllides@gmail.com</a></p>
+        GitHub: <a href="https://github.com/pavlo-tech" target="__blank">pavlo-tech</a><br/>
+        Linkedin: <a href="https://www.linkedin.com/in/pavlo-triantafyllides/" target="__blank">pavlo-triantafyllides</a><br/>
+        Email: <a href="mailto:triantafyllides@gmail.com">triantafyllides@gmail.com</a><br/>
+        </p>
     </div>
   );
 }
