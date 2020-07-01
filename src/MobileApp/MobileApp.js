2f149955702e13a150adf0d7927a74ff0be2a0b9
Pavlo Triantafyllides <triantafyllides@gmail.com>
2020-07-01 18:45:19 -0400

    mobile version ready

--- src/MobileApp/MobileApp.js
+++ src/MobileApp/MobileApp.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useState }  from 'react';
 import './MobileApp.css'
 
-const MobileApp = (props) =>
+const  MobileApp = (props) =>
 {
+  const [secondLeft, setSeconds] = useState(20);
 
-  
+  const decrementSeconds = () => {
+    setSeconds(secondLeft > 0 ? secondLeft-1 : 0);
+  }
+
+  setTimeout(decrementSeconds, 1000)
   return (
     <div className='MobileApp'>
-      This site is not meant to be used on mobile devices
+      <h1>pavlo-tech</h1>
+      <p>
+        Hi there!<br></br> <br></br>
+        My name is Pavlo Triantafyllides, and this is the mobile version of pavlo.tech, my personal website.
+        While a mobile version of this site has not yet been built (and may never be), I would like to thank you for visiting.
+        For the full experience, please visit us (me) on a PC web browser such as Chrome, Firefox, or Safari. 
+        <br></br> <br></br>
+        This message will self destruct in {secondLeft} seconds.
+      </p>
     </div>
   );
 
